455401b672164026c90356e64b9520c02d3c8594
TrinhDinhPhuc <phuccoi996@gmail.com>
2018-02-22 15:45:17 +0700

    first commit

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Twitter App access keys for @phuccoi96	
+
+# Consume:
+CONSUMER_KEY    = 'CrMSxqJz6zvwEa85OOKdUpSeH'
+CONSUMER_SECRET = 'hkdYe8u9SB30Y67uY9RdqRwrh00gZyFedYHI4iyFziRDeVjPxx'
+
+# Access:
+ACCESS_TOKEN  = '3093984751-BpI2Oat6hRf8qEWuIfKlz3Hrn8vsPm3KjOn6GvF'
+ACCESS_SECRET = 'tISJPGvRQ2TZJhKuNA0HZ9kORuS699gsnsVzoh7YV8rUU'
